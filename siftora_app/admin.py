c8b35b57233c0922a6ed8ff1089d84a7cbdd3660
jyaymie <jamiesujiepark@gmail.com>
2022-07-27 08:59:56 -0700

    Create models

--- siftora_app/admin.py
+++ siftora_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Bin, Product
 
-# Register your models here.
+admin.site.register(Bin)
+admin.site.register(Product)
--- siftora_app/models.py
+++ siftora_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Bin(models.Model):
+    title = models.CharField(max_length=50)
+    product_amount = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.title} ({self.product_amount})'
+
+
+class Product(models.Model):
+    brand = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    shade = models.CharField(max_length=50)
+    texture = models.CharField(max_length=50)
+    purchase_date = models.DateField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    open_date = models.DateField()
+    expiry_date = models.DateField()
+    uses = models.PositiveIntegerField()
+    finish_date = models.DateField()
+    will_repurchase = models.BooleanField()
+    notes = models.TextField()
